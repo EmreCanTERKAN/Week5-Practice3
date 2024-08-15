12aeb58cdc8caea9176258252d3301de68e45884
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-08-15 13:28:55 +0300

    Add project files.

---
+++ Week5-Practice3/Program.cs
@@ -0,0 +1,12 @@
+﻿List<string> kahve = new List<string>();
+
+kahve.Add("Mocha");
+kahve.Add("Latte");
+kahve.Add("Esperesso");
+kahve.Add("Americano");
+
+
+for (int i = 0; i < kahve.Count; i++)
+{
+    Console.WriteLine($"Kahve {i+1}: {kahve[i]} ");
+}
